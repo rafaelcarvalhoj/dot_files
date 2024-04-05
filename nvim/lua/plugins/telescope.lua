7432dca324600e470abc6d644abc0d14da90313b
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-04-04 22:06:49 -0300

    filetype tabstop

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
     dependencies = {'nvim-lua/plenary.nvim'},
     config = function()        
         local builtin = require('telescope.builtin')
+        file_ignore_patterns = { "node%_modules/.*" }
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
